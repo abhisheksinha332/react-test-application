6a603e1b94bae098db70abb56cb6d1623769a3e2
Abhishek Sinha <3327abhishek@gmail.com>
2025-09-27 20:47:40 +0530

    Added new version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <h1>This is version 5</h1>
+         <h1>This is version 6</h1>
 
         </p>
         <a
